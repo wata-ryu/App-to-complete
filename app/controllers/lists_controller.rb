17f189722bb66d8074ff866b2fe7c3ba3aff2fa4
wata-ryu <71304058ryu@gmail.com>
2023-05-18 08:24:49 +0000

    バッチ処理学習:23/5/18

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,7 +2,7 @@ class ListsController < ApplicationController
   def new
     @list = List.new
   end
-  
+
   def create
     # １. データを受け取り新規登録するためのインスタンス作成
     list = List.new(list_params)
@@ -12,7 +12,7 @@ class ListsController < ApplicationController
     flash[:notice] = "投稿が成功しました"
     redirect_to list_path(list.id)
   end
-  
+
   #def create
     #@list = List.new(list_params)
     #if @list.save
@@ -21,32 +21,32 @@ class ListsController < ApplicationController
       #render :new
     #end
   #end
-  
-  
+
+
   def index
-    @lists = List.all  
+    @lists = List.all
   end
-  
+
   def show
     @list = List.find(params[:id])
   end
-  
+
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)
   end
-  
+
   def destroy
     list = List.find(params[:id])
     list.destroy
     redirect_to "/lists"
   end
-  
+
   private
     def list_params
       params.require(:list).permit(:title, :body, :image)
